68faf8ebeab809f646aba1471de929a86559d471
Michael Steindorfer <michael@steindorfer.name>
2017-11-11 21:06:44 +0100

    Correcting introduction of contextual tokens for single derivations.

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parseforest/datadependent/DataDependentRuleNode.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parseforest/datadependent/DataDependentRuleNode.java
@@ -23,32 +23,47 @@ public class DataDependentRuleNode extends DataDependentParseForest implements I
         this.parseForests = parseForests;
 
         if (parseForests.length > 0) {
-            if (parseForests[0] instanceof DataDependentSymbolNode) {
-                final DataDependentSymbolNode parseForest = (DataDependentSymbolNode) parseForests[0];
-                final ParseTableProduction leftmostProduction = (ParseTableProduction) parseForest.production;
+            if (parseForests.length == 1) {
+                if (parseForests[0] instanceof DataDependentSymbolNode) {
+                    final DataDependentSymbolNode parseForest = (DataDependentSymbolNode) parseForests[0];
+                    final ParseTableProduction onlyProduction = (ParseTableProduction) parseForest.production;
 
-                // introduction of contextual token
-                contextBitmap |= leftmostProduction.contextL();
+                    // introduction of contextual token
+                    contextBitmap |= onlyProduction.contextL();
+                    contextBitmap |= onlyProduction.contextR();
 
-                // aggregation of recursive contextual tokens
-                for (DataDependentRuleNode ruleNode : parseForest.getDerivations()) {
-                    contextBitmap |= (ruleNode.getContextBitmap());
+                    // aggregation of recursive contextual tokens
+                    for (DataDependentRuleNode ruleNode : parseForest.getDerivations()) {
+                        contextBitmap |= (ruleNode.getContextBitmap());
+                    }
                 }
-            }
-        }
+            } else {
+                if (parseForests[0] instanceof DataDependentSymbolNode) {
+                    final DataDependentSymbolNode parseForest = (DataDependentSymbolNode) parseForests[0];
+                    final ParseTableProduction leftmostProduction = (ParseTableProduction) parseForest.production;
 
-        if (parseForests.length > 1) {
-            if (parseForests[parseForests.length - 1] instanceof DataDependentSymbolNode) {
-                final DataDependentSymbolNode parseForest = (DataDependentSymbolNode) parseForests[parseForests.length - 1];
-                final ParseTableProduction rightmostProduction = (ParseTableProduction) parseForest.production;
+                    // introduction of contextual token
+                    contextBitmap |= leftmostProduction.contextL();
 
-                // introduction of contextual token
-                contextBitmap |= rightmostProduction.contextR();
-
-                // aggregation of recursive contextual tokens
-                for (DataDependentRuleNode ruleNode : parseForest.getDerivations()) {
-                    contextBitmap |= (ruleNode.getContextBitmap());
+                    // aggregation of recursive contextual tokens
+                    for (DataDependentRuleNode ruleNode : parseForest.getDerivations()) {
+                        contextBitmap |= (ruleNode.getContextBitmap());
+                    }
                 }
+
+                if (parseForests[parseForests.length - 1] instanceof DataDependentSymbolNode) {
+                    final DataDependentSymbolNode parseForest = (DataDependentSymbolNode) parseForests[parseForests.length - 1];
+                    final ParseTableProduction rightmostProduction = (ParseTableProduction) parseForest.production;
+
+                    // introduction of contextual token
+                    contextBitmap |= rightmostProduction.contextR();
+
+                    // aggregation of recursive contextual tokens
+                    for (DataDependentRuleNode ruleNode : parseForest.getDerivations()) {
+                        contextBitmap |= (ruleNode.getContextBitmap());
+                    }
+                }
+
             }
         }
     }
